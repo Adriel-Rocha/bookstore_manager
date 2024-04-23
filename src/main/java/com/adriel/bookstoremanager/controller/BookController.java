e9f00189bd0b48e16719d539b638a5b3343f96ff
Adriel Rocha <adrielfr194@gmail.com>
2024-04-23 11:15:35 -0300

    Developed get by id operation

--- src/main/java/com/adriel/bookstoremanager/controller/BookController.java
+++ src/main/java/com/adriel/bookstoremanager/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.adriel.bookstoremanager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +25,14 @@ public class BookController {
         this.bookService = bookService;
     }
 
-
-
     @PostMapping
     public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO) {
         return bookService.create(bookDTO);
     }
     
+    @GetMapping("/{id}")
+    public BookDTO findById(@PathVariable Long id) {
+        return bookService.findById(id);
+    }
+
 }
--- src/main/java/com/adriel/bookstoremanager/service/BookService.java
+++ src/main/java/com/adriel/bookstoremanager/service/BookService.java
@@ -1,5 +1,7 @@
 package com.adriel.bookstoremanager.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +32,11 @@ public class BookService {
         return MessageResponseDTO.builder()
             .message("Book created with ID " + savedBook.getId())
             .build();
+    }
+
+    public BookDTO findById(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        return bookMapper.toDTO(optionalBook.get());
     }
     
 }
